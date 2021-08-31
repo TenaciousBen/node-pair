f6c035277582209ffb2cf72d0bfe2bc34972520b
Ben Hajduk <ben.hajduk@luxtripper.co.uk>
2021-08-31 16:37:48 +0100

    made hotel price basis clearer

--- src/repositories/hotelRepository.ts
+++ src/repositories/hotelRepository.ts
@@ -7,7 +7,7 @@ export interface Hotel {
     id: number;
     name: string;
     starRating: string;
-    pricePerNight: number;
+    pricePerNightPerTraveller: number;
     availability: Availability[]
 }
 
@@ -17,26 +17,26 @@ export interface HotelRepository {
 
 const data: Hotel[] = [
     {
-        id: 1, name: "Cheapo Hotel", starRating: "3.5", pricePerNight: 50, availability: [
+        id: 1, name: "Cheapo Hotel", starRating: "3.5", pricePerNightPerTraveller: 50, availability: [
             { from: "2021-01-01 00:00", to: "2021-03-01 00:00" },
             { from: "2021-05-01 00:00", to: "2021-08-01 00:00" },
             { from: "2021-10-01 00:00", to: "2022-01-01 00:00" },
         ]
     },
     {
-        id: 2, name: "El Average Hotel", starRating: "4", pricePerNight: 75, availability: [
+        id: 2, name: "El Average Hotel", starRating: "4", pricePerNightPerTraveller: 75, availability: [
             { from: "2021-02-01 00:00", to: "2021-04-01 00:00" },
             { from: "2021-05-01 00:00", to: "2021-08-01 00:00" },
             { from: "2021-09-01 00:00", to: "2021-12-01 00:00" },
         ]
     },
     {
-        id: 3, name: "Pricey Hotel", starRating: "4.5", pricePerNight: 100, availability: [
+        id: 3, name: "Pricey Hotel", starRating: "4.5", pricePerNightPerTraveller: 100, availability: [
             { from: "2021-01-01 00:00", to: "2022-01-01 00:00" }
         ]
     },
     {
-        id: 4, name: "Fancy Hotel", starRating: "5", pricePerNight: 200, availability: [
+        id: 4, name: "Fancy Hotel", starRating: "5", pricePerNightPerTraveller: 200, availability: [
             { from: "2021-01-01 00:00", to: "2021-05-01 00:00" },
             { from: "2021-07-01 00:00", to: "2022-01-01 00:00" }
         ]
